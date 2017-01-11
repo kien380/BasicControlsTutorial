6c1db75cc458595997d85ad14108dbb064b94218
KienHHT <huynhhoatrungkien@gmail.com>
2017-01-11 11:28:12 +0700

    Update PopupPage (1)

--- BasicControlsTutorial/Features/PopupPage.xaml.cs
+++ BasicControlsTutorial/Features/PopupPage.xaml.cs
@@ -1,10 +1,4 @@
 ﻿using BasicControlsTutorial.Control;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 
 namespace BasicControlsTutorial.Features
@@ -19,10 +13,58 @@ namespace BasicControlsTutorial.Features
 
         private void InitUI()
         {
-            var popupLayout = new PopupLayout()
-            {
+            
+        }
 
+        /// <summary>
+        /// Create PopupLayout to put in content page
+        /// </summary>
+        /// <returns></returns>
+        private StackLayout CreatePopupContent()
+        {
+            // Create Button Login
+            var btLogin = new Button
+            {
+                Text = "Login"
             };
+            btLogin.Clicked += (s, e) => OnClickBtLogin();
+
+            // Create Popup Content
+            var _PopupContent = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                BackgroundColor = Color.Gray,
+                Padding = 10,
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "Login",
+                        FontSize = 20
+                    },
+
+                    new Entry
+                    {
+                        Placeholder = "Email",
+                        Keyboard = Keyboard.Email
+                    },
+
+                    new Entry
+                    {
+                        Placeholder = "Password",
+                        IsPassword = true
+                    }
+                }
+            };
+
+            return _PopupContent;
+        }
+
+        private void OnClickBtLogin()
+        {
+
         }
     }
 }
