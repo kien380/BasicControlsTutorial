c09883078c3857911897fc67955d9e92886aff34
KienHHT <huynhhoatrungkien@gmail.com>
2017-01-11 11:03:55 +0700

    Create PopupPage

---
+++ BasicControlsTutorial/Features/PopupPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using BasicControlsTutorial.Control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace BasicControlsTutorial.Features
+{
+    public partial class PopupPage : ContentPage
+    {
+        public PopupPage()
+        {
+            InitializeComponent();
+            InitUI();
+        }
+
+        private void InitUI()
+        {
+            var popupLayout = new PopupLayout()
+            {
+
+            };
+        }
+    }
+}
